09beef734f93e55a2c5aaecf4737b2113c820441
dpfrhddh <dpfrhddh@gmail.com>
2020-08-13 15:11:35 +0900

    ADMIN JOIN

---
+++ src/member/command/adminHandler.java
@@ -0,0 +1,35 @@
+package member.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mvc.controller.CommandHandler;
+
+public class adminHandler implements CommandHandler{
+
+	private static final String FORM_VIEW = "/WEB-INF/view/joinForm.jsp";
+	
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		if (req.getMethod().equalsIgnoreCase("GET")) {
+			return processForm(req, res);
+		} else if (req.getMethod().equalsIgnoreCase("POST")) {
+			return processSubmit(req, res);
+		} else {
+			res.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+			return null;
+		}
+	}
+
+	private String processSubmit(HttpServletRequest req, HttpServletResponse res) {
+		String adminPass = req.getParameter("password2");
+		req.getSession().setAttribute("adminPass", adminPass);
+		return FORM_VIEW;
+	}
+
+	private String processForm(HttpServletRequest req, HttpServletResponse res) {
+		// TODO Auto-generated method stub
+		return FORM_VIEW;
+	}
+
+}
